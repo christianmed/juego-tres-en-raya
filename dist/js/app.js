bac3dafb221de2b952e8aa3089b2220b34509a72
Christian Medina <medinachristian1984@gmail.com>
2017-04-23 19:55:56 -0400

    Se agrega boton para reiniar partida, se agrega contador de vitorias a cada jugador y otros cambios de diseño menores

--- dist/js/app.js
+++ dist/js/app.js
@@ -6,10 +6,15 @@ let pX = document.getElementById('pX');
 let pO = document.getElementById('pO');
 let tX = document.getElementById('turnX');
 let tO = document.getElementById('turnO');
+let wX = document.getElementById('winsX');
+let wO = document.getElementById('winsO');
+let vX = 0; // Victories
+let vO = 0; // Victories
 let turn = 1;
 let winner = false;
 let modal = document.getElementById('modal');
 let btnClose = document.getElementById('close');
+let btnReset = document.getElementById('reset');
 let result = document.getElementById('result');
 
 function setTurn(turn) {
@@ -43,6 +48,16 @@ function setWinner(index, letter) {
     winner = true;
     modal.style.display = 'block';
     result.innerHTML = `Ganador ${letter}`;
+
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].removeEventListener('click', tresEnRaya);
+    }
+
+    if (letter === 'X') {
+      wX.innerHTML = ++vX;
+    } else {
+      wO.innerHTML = ++vO;
+    }
   }
 }
 
@@ -67,15 +82,25 @@ function tresEnRaya(e) {
   }
 }
 
+function reset() {
+  for (let i = 0; i < cells.length; i++) {
+    array[i] = '';
+    cells[i].innerHTML = '';
+  }
+  turn = 1;
+  winner = false;
+  ready();
+}
+
 function closeModal() {
   modal.style.display = 'none';
-  setTimeout(() => document.location.reload(), 1000);
 }
 
 function ready() {
   for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', tresEnRaya);
   }
+  btnReset.addEventListener('click', reset);
   btnClose.addEventListener('click', closeModal);
   setTurn(turn);
 }
